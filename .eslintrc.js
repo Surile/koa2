3396368ca1be89a1282cc20863d831d28dca935f
heasy <mhecy98@gmail.com>
2019-01-23 16:59:16 +0800

    配置prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'eslint-config-prettier', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2016,
+    sourceType: 'module'
+  },
+  rules: {
+    curly: ['error', 'all'],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-console':'off'
+  }
+}
